395a98129dca2b8c44d02dec06f452b5d221e70d
MatencaladaDenev <matencalada2002@gmail.com>
2024-05-16 19:12:17 -0500

    Admin v1

---
+++ src/main/java/com/rungroop/login/models/Client.java
@@ -0,0 +1,29 @@
+package com.rungroop.login.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "clientes")
+public class Client {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String nombre;
+  private String apellido;
+  private String pais;
+  private String correo;
+  private String photoUrl;
+  private Integer edad;
+}
